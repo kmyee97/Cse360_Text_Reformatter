86a1f1204ac157db66a91f4e402d76f67c9945c4
james0048 <jdjohnson0048@gmail.com>
2018-03-13 14:18:11 -0700

    Added a terrible GUI and button to call updateFormat method, haven't debugged anything yet or tried it out.

--- reformatter/src/reformatter/Reformatter.java
+++ reformatter/src/reformatter/Reformatter.java
@@ -1,10 +1,104 @@
 package reformatter;
 
-public class Reformatter {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
+import javax.swing.*;
+
+public class Reformatter implements ActionListener {
+
+	//gui stuff
+	JButton openButton;
+	JButton outputButton;
+	JButton formatButton;
+	JFileChooser fc;
+	JFrame frame;
+	JTextField inputName;
+	JTextField outputName;
+	JLabel inLabel;
+	JLabel outLabel;
+	//JTextField outputFileName;
+	
+	//setup vars for input/output files
+	String inputFile;
+	String outputFile;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Reformatter r = new Reformatter();
+		r.MakeGUI();
+		
 
+	}
+	
+	void MakeGUI() {
+		frame = new JFrame();
+		frame.setTitle("Formatter");
+		frame.setSize(300,400);
+		frame.setVisible(true);
+		
+		JPanel contentPane = new JPanel(new BorderLayout());
+		contentPane.setLayout(new GridLayout(2,3));
+		
+		inputName = new JTextField();
+		outputName = new JTextField();
+		fc = new JFileChooser();
+		openButton = new JButton("Open a File");
+		openButton.addActionListener(this);
+		outputButton = new JButton("Select Output Directory");
+		outputButton.addActionListener(this);
+		formatButton = new JButton("Format");
+		formatButton.addActionListener(this);
+		inLabel = new JLabel("Input File: ");
+		outLabel = new JLabel("Output File: ");
+		//outputFileName = new JTextField("Enter output file name here");
+		
+		contentPane.add(openButton);
+		contentPane.add(inLabel);
+		contentPane.add(inputName);
+		
+		contentPane.add(outputButton);
+		contentPane.add(outLabel);
+		contentPane.add(outputName);
+		
+		//contentPane.add(outputFileName);
+		frame.add(contentPane);
+		frame.pack();
+		
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == openButton) {
+			fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			int returnVal = fc.showOpenDialog(frame);
+			if(returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = fc.getSelectedFile();
+				inputFile = file.getAbsolutePath();
+				inputName.setText(inputFile);
+			}
+			
+		}
+		if(e.getSource() == outputButton) {
+			fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			int returnVal = fc.showOpenDialog(frame);
+			if(returnVal == JFileChooser.APPROVE_OPTION) {
+				try {
+					outputFile = fc.getSelectedFile().getCanonicalPath();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				outputName.setText(outputFile);
+			}
+		}
+		if(e.getSource() == formatButton) {
+			UpdateFormat.updateFormat(inputFile, 0, outputFile);
+		}
+		
 	}
 
 }
